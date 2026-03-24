d65e925a08cc50bf7f4ca74572b630ceb53f2c94
ikugast <ikugast@gmail.com>
2026-03-24 15:19:07 +0800

    Delete jindouya_backend_seedai_upload directory

--- jindouya_backend_seedai_upload/start_backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /Users/bytedance/Downloads/jindouya_backend_seedai
-set -a
-source .env
-export FRONTEND_ORIGINS="http://127.0.0.1:5500,http://localhost:5500,https://aidragon.pages.dev"
-set +a
-python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8000
